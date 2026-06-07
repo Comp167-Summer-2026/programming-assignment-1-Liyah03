c0b0e1d1c7cd5087413bcb278393b305a3d4881c
Liyah03 <jnmcdougald@aggies.ncat.edu>
2026-06-07 16:14:06 -0400

    Start of the code

--- src/TemperatureConverter.java
+++ src/TemperatureConverter.java
@@ -1,16 +1,27 @@
+import java.util.Scanner;
+
 public class TemperatureConverter {
 
-    /* hint
-
     public static double convertTemperature(double temperature, String unit) {
-        // TODO: students implement this
+        if (unit.equalsIgnoreCase("C")) {
+            return ((temperature * (9.0 / 5.0)) + 32.0);
+        }
+        else if (unit.equalsIgnoreCase("F")) {
+            return ((temperature - 32.0) * (5.0 / 9.0));
+        }
         return 0.0;
     }
 
-     */
-
     public static void main(String[] args) {
-        // TODO: students implement this
+        Scanner scnr = new Scanner(System.in);
+        String input = "";
+
+        System.out.print("Enter a temperature value (or type 'stop' to quit): ");
+        input = scnr.nextLine();
+
+        while (!input.equalsIgnoreCase("stop")) {
+            
+        }
     }
 
 }
