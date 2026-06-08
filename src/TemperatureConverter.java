a0cce47e197e78aa6c7fc937aab3fc6792c4647a
Liyah03 <jnmcdougald@aggies.ncat.edu>
2026-06-08 00:18:52 -0400

    Final Test

--- src/TemperatureConverter.java
+++ src/TemperatureConverter.java
@@ -27,7 +27,7 @@ public class TemperatureConverter {
                     double temperature = scan.nextDouble();
                     System.out.print("Enter the unit (C or F): ");
 
-                    String unit = scnr.next();
+                    String unit = scnr.nextLine();
 
                     if (unit.equalsIgnoreCase("C") || unit.equalsIgnoreCase("F")) {
                         double convertedTemp = convertTemperature(temperature, unit);
